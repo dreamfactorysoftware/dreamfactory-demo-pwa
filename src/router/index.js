25cee17800e136c13e9ef51f18c3edaea964ed09
Oleksandr Onoshko <oleksandron@softwareplanet.group>
2019-12-12 16:45:12 +0200

    Change basic project structure

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,29 @@
 import Vue from 'vue'
-import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
 
-Vue.use(Router)
+Vue.use(VueRouter)
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Hello',
-      component: Hello
-    }
-  ]
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
 })
+
+export default router
---
+++ vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    pwa: {
+        workboxPluginMode: 'GenerateSW',
+        // workboxOptions: {
+        //     swSrc: 'src/service-worker.js'
+        // },
+        themeColor: '#1da025'
+    },
+}
