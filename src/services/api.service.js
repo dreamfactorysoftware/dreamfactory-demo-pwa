7ba891151646b86aa4f02320642678da2872321c
Oleksandr Onoshko <oleksandron@softwareplanet.group>
2019-12-24 17:43:22 +0200

    DP-128 Create a map view
    - Add new map page
    - Connect google map using vue-google-map library
    - Add logic for displaying employees on map
    - Add clustering

--- src/services/api.service.js
+++ src/services/api.service.js
@@ -7,7 +7,7 @@ const ApiService = {
     departments: [],
     employees: [],
 
-    API_KEY: '36fda24fe5588fa4285ac6c6c2fdfbdb6b6bc9834699774c9bf777f706d05a88',
+    API_KEY: '4f62f758995f8ac02ff0db2887ecdecd282bc7852ffb87524c247daa29c7b72d',
     SESSION_TOKEN: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI4ZWIxYjUyMmY2MGQxMWZhODk3ZGUxZGM2MzUxYjdlOCIsImlzcyI6Imh0dHA6Ly9leGNlbC5zdGFnaW5nLXNwZy5kcmVhbWZhY3RvcnkuY29tL2FwaS92Mi9zeXN0ZW0vYWRtaW4vc2Vzc2lvbiIsImlhdCI6MTU3NjIyNTg5NywiZXhwIjoxNTc5ODI1ODk3LCJuYmYiOjE1NzYyMjU4OTcsImp0aSI6ImhGVU83aW9TVlhkakFERGgiLCJ1c2VyX2lkIjoxLCJmb3JldmVyIjpmYWxzZX0.6aMMemEMvwmLlwdoiyu6fY9FnNznME358DJTgvtVSc0',
     API_URL : 'http://excel.staging-spg.dreamfactory.com/api/v2/mysql/_table',
 
@@ -115,6 +115,22 @@ const ApiService = {
 
     getEmployeesWithPagination(pageSize, offset) {
         return axios.get(`${this.API_URL}/employees?limit=${pageSize}&offset=${offset}`, {
+            dataType: 'json',
+            headers: {
+                'X-DreamFactory-API-Key': this.API_KEY,
+                'X-DreamFactory-Session-Token': this.SESSION_TOKEN
+            }
+        })
+            .then(response => {
+                return response.data.resource;
+            })
+            .catch(e => {
+                console.error(e)
+            })
+    },
+
+    getEmployeesWithZipCoordinates() {
+        return axios.get(`${this.API_URL}/employees?related=zip_coordinates_by_zip`, {
             dataType: 'json',
             headers: {
                 'X-DreamFactory-API-Key': this.API_KEY,
