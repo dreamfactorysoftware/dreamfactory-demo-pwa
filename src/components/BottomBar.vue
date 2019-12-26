edbfc8df59884fc9d7fa309fded2750173f3c6e8
Oleksandr Onoshko <oleksandron@softwareplanet.group>
2019-12-26 17:25:43 +0200

    DP-123 Improve layout and create employee and department directory pages
    - Implement bottom bar using vue material

---
+++ src/components/BottomBar.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="bottom-bar-container">
+    <md-bottom-bar
+      md-sync-route
+    >
+      <md-bottom-bar-item
+        :to="{name: 'home'}"
+        exact
+        md-label="Home"
+        md-icon="home"
+      />
+      <md-bottom-bar-item
+        :to="{name: 'departments'}"
+        md-label="Departments"
+        md-icon="store"
+      />
+      <md-bottom-bar-item
+        :to="{name: 'employees'}"
+        md-label="Employees"
+        md-icon="group"
+      />
+      <md-bottom-bar-item
+        :to="{name: 'map'}"
+        md-label="Map"
+        md-icon="map"
+      />
+    </md-bottom-bar>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "BottomBar"
+    }
+</script>
+
+<style scoped lang="scss">
+  .md-bottom-bar-item {
+    text-decoration: none;
+  }
+</style>
