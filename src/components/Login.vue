5c468bf568d5f8b8df95d46caea39e7263a4ffb4
Yaroslav Mokhurenko <yaroslavmo@softwareplanet.uk.com>
2020-01-08 18:00:04 +0200

    DP-134 Implement admin functionality
    - Add LOGIN_URL env variable

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -6,7 +6,7 @@
     <div class="login-form">
       <div class="form-button-row">
         <a
-          href="http://excel.staging-spg.dreamfactory.com/api/v2/okta_sso/sso"
+          :href="loginUrl"
           class="signin-button btn btn-primary"
         >Sign in with okta</a>
       </div>
@@ -16,7 +16,12 @@
 
 <script>
     export default {
-        name: "Login"
+        name: "Login",
+        data() {
+          return {
+            loginUrl: process.env.VUE_APP_LOGIN_URL,
+        }
+      },
     }
 </script>
 
